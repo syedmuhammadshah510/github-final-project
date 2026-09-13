65a44183fd48a54b6b5d8a21cec8a972b479c87d
Muhammad Shah <syedmuhammadshah510@gmail.com>
2026-09-13 11:17:33 +0500

    Fix typo

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+echo "Simple Interest Calculator"
+
+read -p "Enter Principal: " principal
+read -p "Enter Rate: " rate
+read -p "Enter Time: " time
+
+interest=$((principal * rate * time / 100))
+
+echo "Simple Interest = $interest"
